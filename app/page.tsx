db9ee60d44934c8ba06ede7281a1f19900bc92fe
dishwasher-detergent <kenny_bass@outlook.com>
2023-01-31 19:56:44 -0600

    temporarily add json of projects to get content on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 import Project from "#/ui/Project";
+import projects from "#/_projects/projects.json";
+
+export type Projects = {
+  title: string;
+  short_description: string;
+  description: string;
+  banner: string;
+  images: string[];
+  tags: string[];
+};
 
 export default function Home() {
   return (
     <section className="w-full h-full overflow-hidden rounded-xl grid grid-cols-1 md:grid-cols-2 project-grid">
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
+      {projects.map((item: Projects) => {
+        return <Project key={item.title} content={item} />;
+      })}
     </section>
   );
 }
--- ui/Project/index.tsx
+++ ui/Project/index.tsx
@@ -1,37 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { Projects } from "#/app/page";
 
-export default function Project() {
+interface ProjectProps {
+  content: Projects;
+}
+
+export default function Project({ content }: ProjectProps) {
   return (
     <article className="h-72 bg-white relative overflow-hidden group">
       <Link
-        href="/"
+        href={`projects/${content.title.toLowerCase()}`}
         className="absolute inset-0 z-30 bg-slate-900/30 backdrop-blur-md opacity-0 group-hover:opacity-100 transition-all grid place-items-center"
       >
         <button className="py-2 px-4 rounded-xl text-white group-hover:bg-slate-900 flex flex-row gap-2 items-center text-sm font-semibold">
           Explore Project <ArrowRight size={20} />
         </button>
       </Link>
-      <div className="absolute bottom-0 z-20 p-2 w-full">
+      <div className="absolute bottom-0 z-20 p-4 w-full">
         <h3 className="w-full truncate font-bold text-xl display text-white dark:text-slate-900">
-          Titleasdfasdfasdfasdfasdfasdfasdfasdfa sdfasdfasdf as df
+          {content.title}
         </h3>
         <p className="text-sm text-white dark:text-slate-900 truncate w-full">
-          Descriptionasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
+          {content.short_description}
         </p>
         <div className="flex flex-row flex-nowrap py-2 gap-1 overflow-x-auto overflow-y-hidden">
-          <div className="whitespace-nowrap rounded-full px-1.5 py-0.5 text-xs font-bold text-white bg-emerald-600">
-            Test
-          </div>
+          {content.tags.map((item: string, index: number) => {
+            return (
+              <div
+                className="whitespace-nowrap rounded-full px-1.5 py-0.5 text-xs font-semibold text-white bg-emerald-600"
+                key={index}
+              >
+                {item}
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="z-10 absolute inset-0 bg-gradient-to-t from-slate-900/90 to-transparent dark:from-slate-300/70" />
+      <div className="z-10 absolute inset-0 bg-gradient-to-t from-slate-900/80 to-transparent dark:from-slate-300/70" />
       <div className="bg-slate-900 absolute inset-0 z-0">
         <Image
           style={{ objectFit: "cover", objectPosition: "0% 15%" }}
           fill={true}
-          src={`https://images.unsplash.com/photo-1503777119540-ce54b422baff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80`}
+          src={`${content.banner}`}
           alt="This is the preview image of the component"
         />
       </div>
