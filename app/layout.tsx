e449e47061f0190c2e3217fde0cbdb2d9d4ce76f
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-02-20 10:07:59 -0600

    Fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`bg-gradient-to-b from-white to-slate-400 dark:from-slate-900 dark:to-slate-400 ${font} ${displayFont}`}>
+      <body className={`bg-gradient-to-b from-white to-slate-400 dark:from-slate-900 dark:to-slate-400 ${font.className} ${displayFont.variable}`}>
         <Layout>{children}</Layout>
         <ToastWrapper />
       </body>
