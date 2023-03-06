d42af7ea013fe554681b12eadb3e75b58c9b439f
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-03-06 10:50:09 -0600

    Update PresenceWrapper.tsx

--- ui/animate/PresenceWrapper.tsx
+++ ui/animate/PresenceWrapper.tsx
@@ -3,5 +3,5 @@
 import { AnimatePresence } from "framer-motion";
 
 export function PresenceWrapper({ children }: { children: React.ReactNode }) {
-  return <AnimatePresence initial={false} mode="popLayout">{children}</AnimatePresence>;
+  return <AnimatePresence initial={false} mode="wait" onExitComplete={() => window.scrollTo({top: 0})}>{children}</AnimatePresence>;
 }
