8bd23b1c524f53a330879b5e1beab6f326536d9f
Kenneth Bass <kenny_bass@outlook.com>
2023-11-04 17:41:30 -0500

    add project links

--- components/projects/project.tsx
+++ components/projects/project.tsx
@@ -1,4 +1,5 @@
 import { AW_PROJECT_BUCKET_ID, AW_PROJECT_ID } from "@/utils/appwrite";
+import { LucideExternalLink, LucideGithub } from "lucide-react";
 import { Corners } from "../corners";
 
 interface ProjectProps {
@@ -21,14 +22,14 @@ export function Project({
   tags,
 }: ProjectProps) {
   return (
-    <a className="relative w-full md:h-60 flex flex-col md:flex-row md:items-center group cursor-pointer p-4">
+    <div className="relative w-full md:h-60 flex flex-col md:flex-row md:items-center group cursor-pointer p-4">
       <div className="flex-none md:h-full w-full aspect-square md:aspect-auto md:w-0 bg-white/20 rounded-3xl md:group-hover:w-80 md:group-hover:mr-6 transition-all overflow-hidden">
         <img
           src={`https://cloud.appwrite.io/v1/storage/buckets/${AW_PROJECT_BUCKET_ID}/files/${images[0]}/view?project=${AW_PROJECT_ID}`}
           className="h-full w-full object-cover object-left-top"
         />
       </div>
-      <div className="flex-1 overflow-hidden space-y-2 py-2 md:py-0 md:space-y-0">
+      <div className="flex-1 overflow-hidden space-y-2 py-2 md:py-0 z-50">
         <p className="text-5xl md:text-8xl font-display truncate">{title}</p>
         <div className="flex flex-row flex-wrap gap-2">
           {tags.map((tag) => (
@@ -40,8 +41,28 @@ export function Project({
             </span>
           ))}
         </div>
+        <div className="flex flex-row flex-wrap gap-2">
+          {website && (
+            <a
+              href={website}
+              target="_blank"
+              className="p-2 rounded-xl hover:bg-white/20"
+            >
+              <LucideExternalLink size={24} />
+            </a>
+          )}
+          {github && (
+            <a
+              href={github}
+              target="_blank"
+              className="p-2 rounded-xl hover:bg-white/20"
+            >
+              <LucideGithub size={24} />
+            </a>
+          )}
+        </div>
       </div>
       <Corners />
-    </a>
+    </div>
   );
 }
