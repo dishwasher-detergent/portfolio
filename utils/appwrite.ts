1252ec19d591506d61a5613186d12052c98cefe0
Kenneth Bass <kenny_bass@outlook.com>
2023-11-04 17:17:46 -0500

    update env

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -2,9 +2,11 @@ import { ProjectType } from "@/types/types";
 import { Client, Databases, Query } from "appwrite";
 
 export const AW_ENDPOINT =
-  process.env.APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1";
-export const AW_PROJECT_ID = process.env.APPWRITE_PROJECT_ID as string;
-export const AW_DATABASE_ID = process.env.APPWRITE_DATABASE_ID as string;
+  process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1";
+export const AW_PROJECT_ID = process.env
+  .NEXT_PUBLIC_APPWRITE_PROJECT_ID as string;
+export const AW_DATABASE_ID = process.env
+  .NEXT_PUBLIC_APPWRITE_DATABASE_ID as string;
 export const AW_PROJECT_COLLECTION_ID = "63e17a3b092917cea721";
 export const AW_PROJECT_BUCKET_ID = "63e17bd7024f7fadf59d";
 
