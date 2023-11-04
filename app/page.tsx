a750a924f7d5d5dab7971722e0024c06da118ffc
Kenneth Bass <kenny_bass@outlook.com>
2023-11-04 17:44:23 -0500

    fix main text overlap

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import { ChevronDown } from "lucide-react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="w-full h-[50vh] md:h-[75vh] p-6 md:p-12 relative">
-        <div className="w-full h-full flex flex-col justify-center items-center bg-gradient-to-br from-white/50 to-transparent rounded-[3rem] md:rounded-[6rem] text-white relative z-10">
-          <div className="absolute top-0 w-full flex items-center justify-center">
+      <div className="w-full md:h-[75vh] p-6 md:p-12 relative">
+        <div className="w-full h-full flex flex-col justify-between md:justify-center items-center bg-gradient-to-br from-white/50 to-transparent rounded-[3rem] md:rounded-[6rem] text-white relative z-10">
+          <div className="md:absolute md:top-0 w-full flex items-center justify-center">
             <Logo />
           </div>
-          <h1 className="font-display lg:text-8xl md:text-7xl text-6xl">
+          <h1 className="font-display lg:text-8xl md:text-7xl text-6xl py-24">
             <span className="md:space-x-3">
               <span>Building</span>
               <br className="md:hidden" />
@@ -23,7 +23,7 @@ export default function Home() {
               <span className="hidden md:inline-block h-2 flex-1 bg-white rounded-full" />
             </span>
           </h1>
-          <div className="absolute bottom-0 w-full flex items-center justify-center">
+          <div className="md:absolute md:bottom-0 w-full flex items-center justify-center h-24 md:h-32">
             <a className="animate-bounce cursor-pointer">
               <ChevronDown className="w-12 h-12" strokeWidth={3} />
             </a>
