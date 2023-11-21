891ce4ebd032993226534a6c9ef8df2ec4086d5d
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-11-21 14:21:11 -0600

    Update index.tsx

--- components/marquee/index.tsx
+++ components/marquee/index.tsx
@@ -5,7 +5,7 @@ interface MarqueeProps {
 export function Marquee({ text }: MarqueeProps) {
   return (
     <section className="py-4">
-      <div className="relative flex overflow-hidden select-none gap-4 md:text-8xl text-6xl font-black text-white py-4">
+      <div className="relative flex overflow-hidden select-none gap-4 md:text-6xl text-4xl font-black text-white py-4">
         <ul className="flex-shrink-0 flex justify-around gap-4 min-w-full marquee">
           {[...Array(10)].map((index: number) => (
             <li key={index}>{text}</li>
@@ -21,7 +21,7 @@ export function Marquee({ text }: MarqueeProps) {
           ))}
         </ul>
       </div>
-      <div className="relative flex overflow-hidden select-none gap-4 md:text-8xl text-6xl font-black text-white py-4 font-display">
+      <div className="relative flex overflow-hidden select-none gap-4 md:text-6xl text-4xl font-black text-white py-4 font-display">
         <ul className="flex-shrink-0 flex justify-around gap-4 min-w-full marquee reverse">
           {[...Array(10)].map((index: number) => (
             <li key={index}>{text}</li>
