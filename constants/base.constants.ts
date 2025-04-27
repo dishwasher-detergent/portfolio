0eda3aa93df150a84bd4fc7c1560f83d188a3513
Kenneth Bass <kenny_bass@outlook.com>
2025-04-27 10:42:29 -0500

    fix path

--- constants/base.constants.ts
+++ constants/base.constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://6808275830d4efb30a3f.nyc.appwrite.run/organizations/6771fb5e00156e3c9638";
+  "https://6808275830d4efb30a3f.nyc.appwrite.run/teams/6771fb5e00156e3c9638";
